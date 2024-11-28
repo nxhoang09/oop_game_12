352ac4f1fe97d3e82684d61ab920eb58b26fed82
glee-cvp <truonggiangcvp@gmail.com>
2024-11-29 02:05:49 +0700

    qsjava

--- oop_12/src/gamestates/QS.java
+++ oop_12/src/gamestates/QS.java
@@ -1,5 +1,112 @@
 package gamestates;
 
-public class QS {
-    
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import main.Game;
+import utilz.LoadSave;
+
+public class QS extends State implements Statemethods {
+
+    private BufferedImage backgroundImg;
+    private ArrayList<ShowEntity> entitiesList;
+
+    public QS(Game game) {
+        super(game);
+        backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.QS_menu);
+        loadEntities();
+    }
+
+    private void loadEntities() {
+        entitiesList = new ArrayList<>();
+        entitiesList.add(new ShowEntity(getIdleAni(LoadSave.GetSpriteAtlas(LoadSave.PLAYER_PIRATE), 5, 64, 40), (int) (Game.GAME_WIDTH * 0.60), (int) (Game.GAME_HEIGHT * 0.55)));
+        entitiesList.add(new ShowEntity(getIdleAni(LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE), 9, 72, 32), (int) (Game.GAME_WIDTH * 0.70), (int) (Game.GAME_HEIGHT * 0.55)));
+        entitiesList.add(new ShowEntity(getIdleAni(LoadSave.GetSpriteAtlas(LoadSave.PINKSTAR_ATLAS), 8, 34, 30), (int) (Game.GAME_WIDTH * 0.65), (int) (Game.GAME_HEIGHT * 0.75)));
+        entitiesList.add(new ShowEntity(getIdleAni(LoadSave.GetSpriteAtlas(LoadSave.SHARK_ATLAS), 8, 34, 30), (int) (Game.GAME_WIDTH * 0.75), (int) (Game.GAME_HEIGHT * 0.75)));
+    }
+
+    private BufferedImage[] getIdleAni(BufferedImage atlas, int spritesAmount, int width, int height) {
+        BufferedImage[] arr = new BufferedImage[spritesAmount];
+        for (int i = 0; i < spritesAmount; i++)
+            arr[i] = atlas.getSubimage(width * i, 0, width, height);
+        return arr;
+    }
+
+    @Override
+    public void update() {
+        
+        for (ShowEntity se : entitiesList)
+            se.update();
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.drawImage(backgroundImg, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
+        for (ShowEntity se : entitiesList)
+            se.draw(g);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+          
+            setGamestate(Gamestate.MENU);
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    private class ShowEntity {
+        private BufferedImage[] idleAnimation;
+        private int x, y, aniIndex, aniTick;
+
+        public ShowEntity(BufferedImage[] idleAnimation, int x, int y) {
+            this.idleAnimation = idleAnimation;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void draw(Graphics g) {
+            g.drawImage(idleAnimation[aniIndex], x, y, (int) (idleAnimation[aniIndex].getWidth() * 4), (int) (idleAnimation[aniIndex].getHeight() * 4), null);
+        }
+
+        public void update() {
+            aniTick++;
+            if (aniTick >= 25) {
+                aniTick = 0;
+                aniIndex++;
+                if (aniIndex >= idleAnimation.length)
+                    aniIndex = 0;
+            }
+
+        }
+    }
+
 }
