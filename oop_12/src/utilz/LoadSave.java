9542bb64fcf7f2b8bdc829fb7cd06b923de0f501
glee-cvp <truonggiangcvp@gmail.com>
2024-12-03 23:10:26 +0700

    menubackground

--- oop_12/src/utilz/LoadSave.java
+++ oop_12/src/utilz/LoadSave.java
@@ -26,7 +26,7 @@ public class LoadSave {
     public static final String QS_BUTTON = "QSimg.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
     public static final String MENU_BUTTONS = "button_atlas.png";
-    public static final String MENU_BACKGROUND = "menu_background.png";
+    public static final String MENU_BACKGROUND = "menubackground.png";
     public static final String PAUSE_BACKGROUND = "pause_menu.png";
     public static final String SOUND_BUTTONS = "sound_button.png";
     public static final String URM_BUTTONS = "urm_buttons.png";
