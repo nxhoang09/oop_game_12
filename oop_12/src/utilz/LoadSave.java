ba37a56fb9cb2f35bae7f6b33ee079f65435be82
glee-cvp <truonggiangcvp@gmail.com>
2024-12-03 23:07:03 +0700

    background

--- oop_12/src/utilz/LoadSave.java
+++ oop_12/src/utilz/LoadSave.java
@@ -31,7 +31,7 @@ public class LoadSave {
     public static final String SOUND_BUTTONS = "sound_button.png";
     public static final String URM_BUTTONS = "urm_buttons.png";
     public static final String VOLUME_BUTTONS = "volume_buttons.png";
-    public static final String MENU_BACKGROUND_IMG = "background_menu.png";
+    public static final String MENU_BACKGROUND_IMG = "background.png";
     public static final String PLAYING_BG_IMG = "playing_bg_img.png";
     public static final String LAYER_IMG = "layer_img.png";
     public static final String SMALL_CLOUDS = "small_clouds.png";
